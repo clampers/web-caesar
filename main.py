3eb9a50d114b8d62913aed55ef03dcfe12ba9e46
Andrew Barrett <brazenbarrett@gmail.com>
2017-09-23 09:52:32 -0500

    Added formating for form routes

--- main.py
+++ main.py
@@ -10,26 +10,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" value="0"><br>
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit" value="Submit Query">
         </form>
     </body>
@@ -50,11 +50,11 @@ def encrypt():
 
     encrypted_message = rotate_string(message, rot_by)
 
-    return encrypted_message
+    return form.format(encrypted_message)
 
 
 @app.route("/")
 def index():
-    return form
+    return form.format()
 
 app.run()
